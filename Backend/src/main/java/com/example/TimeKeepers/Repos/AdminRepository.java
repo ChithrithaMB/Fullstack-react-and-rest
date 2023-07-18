0cf5260833847c0ca0897d9ed761c2a69cf7898f
ChithrithaMB <119107086+ChithrithaMB@users.noreply.github.com>
2023-07-18 12:03:36 +0530

    Delete Backend directory

--- Backend/src/main/java/com/example/TimeKeepers/Repos/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TimeKeepers.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.TimeKeepers.Model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long>{
-	
-}
--- Backend/src/main/java/com/example/TimeKeepers/Repos/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TimeKeepers.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.TimeKeepers.Model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-	
-}
